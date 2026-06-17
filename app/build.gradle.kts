e1565a91905e4d46c2b0d7f23ce63611d08110de
BrunonToJa <127970288+BrunonToJa@users.noreply.github.com>
2026-06-17 18:30:02 +0200

    Remove Google Services plugin from app Gradle config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("com.google.gms.google-services")
 }
 
 android {
@@ -42,34 +41,27 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
-    // Google
+    // Google Sign-In
     implementation("com.google.android.gms:play-services-auth:20.7.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     // Retrofit do obsługi API
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
+
     // Coroutines (do operacji w tle)
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+
     // Bezpieczne przechowywanie kluczy (Security Crypto)
     implementation("androidx.security:security-crypto:1.1.0-alpha06")
 
     implementation("androidx.cardview:cardview:1.0.0")
-    implementation("com.google.android.material:material:1.9.0")
 
     // Charts.kt - biblioteka wykresów
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 
-    // XTB
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+    // XTB / sieć
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
 
     implementation("androidx.recyclerview:recyclerview:1.3.2")
